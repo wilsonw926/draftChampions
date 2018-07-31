2a8495b71937880edfbc4ca9a6a4cf2bfcbe8ac3
Wilson Wang <wilsonw926@gmail.com>
2018-07-30 17:09:08 -0700

    DS Store and qb pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Users
 User.create(email: 'admin@gmail.com', password: 'password', password_confirmation: 'password', admin: true)
+User.create(email: 'user@gmail.com', password: 'password', password_confirmation: 'password', admin: false)
+
+#Players
+
+#QBs
+p = Player.create(name: 'Michael Vick', position: 'QB', overall: 99)
+p.front_card.attach(io: File.open('app/assets/images/michael_vick_f.png'), filename: 'michael_vick_f.png')
+p.back_card.attach(io: File.open('app/assets/images/michael_vick_b.png'), filename: 'michael_vick_b.png')
+
+p = Player.create(name: 'Aaron Rodgers', position: 'QB', overall: 97)
+p.front_card.attach(io: File.open('app/assets/images/aaron_rodgers_f.png'), filename: 'aaron_rodgers_f.png')
+p.back_card.attach(io: File.open('app/assets/images/aaron_rodgers_b.png'), filename: 'aaron_rodgers_b.png')
+
+p = Player.create(name: 'Tom Brady', position: 'QB', overall: 96)
+p.front_card.attach(io: File.open('app/assets/images/tom_brady_f.png'), filename: 'tom_brady_f.png')
+p.back_card.attach(io: File.open('app/assets/images/tom_brady_b.png'), filename: 'tom_brady_b.png')
